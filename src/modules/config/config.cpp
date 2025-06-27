90150f70c45c0a3d2682d81112def6e666378ad1
Maksim <maksimshchavelev@gmail.com>
2025-06-27 21:00:19 +0300

    fix: initialization of variable Config structure now in a separate .cpp file for the correct linking

---
+++ src/modules/config/config.cpp
@@ -0,0 +1,11 @@
+/**
+ * @file config.hpp
+ * @brief Configuration file
+ * @license MIT LICENSE, Copyright (c) 2025, maksimshchavelev
+ */
+
+
+#include "config.hpp"
+
+
+uint16_t Config::port = 14540;
