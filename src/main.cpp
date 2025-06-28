0326cbc8083dd8f7f18aced810a23d0c26a79d17
Maksim <maksimshchavelev@gmail.com>
2025-06-28 11:24:28 +0300

    feat: initial adding of app module

--- src/main.cpp
+++ src/main.cpp
@@ -1,34 +1,18 @@
 /* MIT LICENSE, Copyright (c) 2025, maksimshchavelev */
 
 
-#include "modules/discovery/discovery.hpp"
-#include "modules/transport/jsontransport/jsontransport.hpp"
+#include <signal.h>
+#include "modules/app/app.hpp"
+
+
+void exit_signal(int sig) { app().exit(); }
 
 
 int main(int argc, char** argv)
 {
-    Discovery& disc = Discovery::get_instance();
+    signal(SIGINT, exit_signal);
 
-    JsonProtocol protocol;
-    JsonTransport jsontransport(protocol);
-
-    jsontransport.run_receiving_messages_async([](Message msg){
-        std::cout << from_byteVector<std::string>(msg.data) << std::endl;
-        std::cout << from_byteVector<std::string>(msg.sender) << std::endl;
-
-        std::time_t utc = std::chrono::system_clock::to_time_t(msg.send_time);
-        std::cout << std::put_time(std::localtime(&utc), "%e.%m.%Y %H.%M") << std::endl << std::endl;
-    });
-
-    jsontransport.send_message("192.168.0.100", Message{
-        .sender = to_byteVector(std::string("Maksim")),
-        .data = to_byteVector(std::string("Проверка связи")),
-        .send_time = std::chrono::system_clock::now()
-    });
-
-    std::cout << std::chrono::duration_cast<std::chrono::minutes>(std::chrono::system_clock::now().time_since_epoch()).count();
-
-    drogon::app().run();
+    app().run();
 
     return 0;
 }
